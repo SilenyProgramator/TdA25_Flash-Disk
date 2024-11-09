7c37c763436abc2375e29bf199a9bea2d9023dc2
Pampkin128 <99970047+Pampkin128@users.noreply.github.com>
2024-11-09 12:40:08 +0100

    Oprava api, možná

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/api', (req, res) => {
+  res.json({ organization: "Space Cyber Games" });
 });
 
 module.exports = router;
