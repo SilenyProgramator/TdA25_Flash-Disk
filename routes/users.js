e149ba6b82638bc9568a3930642046c2b90a5185
Pampkin128 <99970047+Pampkin128@users.noreply.github.com>
2024-11-09 12:02:50 +0100

    apimk3
    api mk3

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Define a simple API endpoint
+router.get('/api', (req, res) => {
+  res.json({ organization: "Student Cyber Games" });
 });
 
 module.exports = router;
