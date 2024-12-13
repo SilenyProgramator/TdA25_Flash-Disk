5b51ed7f44809638901e37517e5191770962ca51
SilenyProgramator <programatorsileny@gmail.com>
2024-12-13 21:17:02 +0100

    dalsi uprava- misto OK to bude zobrazovat to co mam ve slozce public

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendStatus(200); // posle status kod 200 pri GET requestu na "/"
+  res.sendFile(path.join(__dirname, '../public', 'main.html')); // posle status kod 200 pri GET requestu na "/"
 });
 
 module.exports = router;
 
-<p>ahoj</p>
+
