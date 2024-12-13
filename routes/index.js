4596648181204b20cf820bab2440135c00c3bebd
SilenyProgramator <programatorsileny@gmail.com>
2024-12-13 21:21:41 +0100

    +path

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path'); // Import modulu path
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'main.html')); // posle status kod 200 pri GET requestu na "/"
